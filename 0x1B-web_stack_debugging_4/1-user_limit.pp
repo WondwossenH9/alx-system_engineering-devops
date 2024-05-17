1829bdebe3746292125af1e5ac1f44d86824d02e
WondwossenH9 <wondwossenhailu7@gmail.com>
2024-05-17 02:05:32 -0700

    create files for web stack debugging 4 project

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Changes OS config for login with
+# holberton user and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
